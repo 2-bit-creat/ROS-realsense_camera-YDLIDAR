7464be0f6baf0d154ca63b6bae2086b5a52fbc3b
2-bit-creat <81176520+2-bit-creat@users.noreply.github.com>
2021-09-14 17:38:48 +0900

    Update flag_publisher.py

--- self_driving/scripts/flag_publisher.py
+++ self_driving/scripts/flag_publisher.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Int32
+from std_msgs.msg import Float32
 import rospy
 import cv2
 import serial
@@ -15,19 +16,15 @@ class rplidar_sub:
     
     def __init__(self):
         rospy.init_node("flag_node")
-        self.sub = rospy.Subscriber("/scan", LaserScan, self.callback1)
+        self.sub = rospy.Subscriber("/scan", LaserScan, self.callback3)
         self.pub = rospy.Publisher('/flag', Int32, queue_size= 5)
-        print("hello")
-        rospy.spin() #if there is no rospy.spin the process will end right away. But in case there are other loops that enables the process keep going, you don't have to use rospy.spin
-
-    def callback1(self, rp_data):
-        for i in range(0, 29, 1):
-            if rp_data.ranges[29-i] < 0.7 or rp_data.ranges[330+i] < 0.7: #publish 1 when the LiDAR detects something within 0.7m in the range of [0, 30] or [330, 360] degrees.
+ 
+    def callback3(self, rp_data):
+        for i in range(300, 450, 2):
+            if (rp_data.ranges[i] < 0.7 and rp_data.ranges[i] != 0):  #publish 1 when the LiDAR detects something within 0.7m in the range of [0, 30] or [330, 360] degrees.
                 self.pub.publish(1)
                 return 
-            
-    def callback2(self, rp_data):
-        print("0: ", rp_data.range[0], "90: ",rp_data.range[90], "180: ", rp_data.range[180], "270: ", rp_data.range[270])
+        self.pub.publish(0)
         
 
 if __name__ == "__main__":
